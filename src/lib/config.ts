6078cfcd9ccab884949ab5d300b808fc13581679
Vickymane345 <113917597+Vickymane345@users.noreply.github.com>
2026-03-18 22:27:19 +0100

    first commit

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,32 +1,37 @@
+import image1 from "@/app/assets/wallets/btc.png";
+import image2 from "@/app/assets/wallets/tether.png";
+import image3 from "@/app/assets/wallets/etherium.png";
+import image4 from "@/app/assets/wallets/xrp.png";
+import image5 from "@/app/assets/wallets/solana.jpg";
 export const WALLETS = [
   {
     name: 'Bitcoin',
-    symbol: 'BTC',
+    symbol: image1,
     address: process.env.NEXT_PUBLIC_BTC_ADDRESS || 'bc1q0dly0gy6zvsqudx7cxh56auwjhg43wylt4xwya',
   },
   {
     name: 'Tether (ERC20)',
-    symbol: 'USDT',
+    symbol: image2,
     address: process.env.NEXT_PUBLIC_USDT_ERC20_ADDRESS || '0x3e09cE4993814b438e220bE2bD04e5fd4C166179',
   },
   {
     name: 'Ethereum',
-    symbol: 'ETH',
+    symbol: image3,
     address: process.env.NEXT_PUBLIC_ETH_ADDRESS || '0x3e09cE4993814b438e220bE2bD04e5fd4C166179',
   },
   {
     name: 'Ripple',
-    symbol: 'XRP',
+    symbol: image4,
     address: process.env.NEXT_PUBLIC_XRP_ADDRESS || 'rag4voiXTrLszwWVQDngtABMMBZTgBzWsM',
   },
   {
     name: 'Solana',
-    symbol: 'SOL',
+    symbol: image5,
     address: process.env.NEXT_PUBLIC_SOL_ADDRESS || '9fY3jXfbb4KQ6yzorKRqaKF2hzKz4jT4oFhfd4j3Kn78',
   },
   {
     name: 'Tether (BEP20)',
-    symbol: 'USDT-BSC',
+    symbol: image2,
     address: process.env.NEXT_PUBLIC_USDT_BEP20_ADDRESS || '0x3e09cE4993814b438e220bE2bD04e5fd4C166179',
   },
 ];
@@ -36,5 +41,3 @@ export const EMAILJS_CONFIG = {
   templateId: process.env.EMAILJS_TEMPLATE_ID,
   publicKey: process.env.EMAILJS_PUBLIC_KEY,
 };
-
-
